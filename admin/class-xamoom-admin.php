b2f1d50b0397069ac0b38192f0948576e5af9936
Bruno Hautzenberger <bhautzenberger@gmail.com>
2015-07-29 16:19:37 +0200

    added API verification

--- admin/class-xamoom-admin.php
+++ admin/class-xamoom-admin.php
@@ -134,6 +134,38 @@ class xamoom_Admin {
 	 */
 	public function show_settings_page(){
 		?>
+		<script Language="JavaScript">
+			function testAPIKey(){
+					//just request one content item (a fast call to test if everything works)
+					var params = { page_size: "1" };
+
+					//get content to update title and excerpt
+					jQuery.ajax({
+							contentType: 'application/json',
+							dataType: 'json',
+							headers: {"Authorization":jQuery('#xamoom_api_key').val()},
+							success: function(data){
+																			jQuery('#xamoom_api_key').css('border-style','solid');
+																			jQuery('#xamoom_api_key').css('border-color','#00ff00');
+																		},
+							error: function(){
+																jQuery('#xamoom_api_key').css('border-style','solid');
+																jQuery('#xamoom_api_key').css('border-color','#ff0000');
+															},
+							type: 'GET',
+							url: xamoom_api_endpoint  + 'content?' + jQuery.param(params)
+					});
+			}
+
+			jQuery( document ).ready(function() {
+    		console.log( "ready!" );
+				testAPIKey(); //test API key on page load
+				jQuery('#xamoom_api_key').focusout(function() {
+			    testAPIKey();
+			  });
+			});
+		</script>
+
 		<div class="wrap">
 			<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
 			<form method="post" action="options.php">
@@ -164,6 +196,7 @@ class xamoom_Admin {
 				<?php submit_button(); ?>
 			</form>
 		</div>
+
 		<?php
 	}
 }
