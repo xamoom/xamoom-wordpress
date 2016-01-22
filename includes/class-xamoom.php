07c6c6a52bb09268c292d0f7a6e97092e0a6dd08
Bruno Hautzenberger <bruno@xamoom.com>
2016-01-22 12:50:14 +0100

    production version

--- includes/class-xamoom.php
+++ includes/class-xamoom.php
@@ -68,9 +68,9 @@ class xamoom {
 	public function __construct() {
 
 		$this->plugin_name = 'xamoom';
-		$this->version = '1.0.2';
-		#$this->api_endpoint = 'https://xamoom-api-dot-xamoom-cloud.appspot.com/_ah/api/xamoomIntegrationApi/v1/';
-		$this->api_endpoint = 'https://22x-dot-xamoom-api-dot-xamoom-cloud-dev.appspot.com/_api/v2/integration/';
+		$this->version = '1.4.3';
+		$this->api_endpoint = 'https://xamoom-api-dot-xamoom-cloud.appspot.com/_ah/api/xamoomIntegrationApi/v1/';
+		#$this->api_endpoint = 'https://22x-dot-xamoom-api-dot-xamoom-cloud-dev.appspot.com/_api/v2/integration/';
 
 		$this->load_dependencies();
 		$this->set_locale();
