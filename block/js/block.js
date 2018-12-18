d538c54858d86b824adf6279c130d08d9e3bb312
Bruno Hautzenberger <bruno@xamoom.com>
2018-12-18 08:19:15 +0100

    added xamoom logo svg

--- block/js/block.js
+++ block/js/block.js
@@ -41,17 +41,15 @@ function xamoomSelectContent(content_id){
 
 
 ( function() {
-	var __ = wp.i18n.__; // The __() for internationalization.
-	var el = wp.element.createElement; // The wp.element.createElement() function to create elements.
-	var registerBlockType = wp.blocks.registerBlockType; // The registerBlockType() to register blocks.
+	var __ = wp.i18n.__;
+	var el = wp.element.createElement;
+	var registerBlockType = wp.blocks.registerBlockType;
 
 	// the current search cursor for page queries.
     var xamoom_search_cursor = "";
 
     //currently set search options
     var xamoom_search_last_search_params = null;
-
-
 
     /**
      * Initializes a new search (page 1) for content pages in the xamoom backend
@@ -180,9 +178,13 @@ function xamoomSelectContent(content_id){
         });
     }
 
+    const iconEl = el('svg', { width: 24, height: 24 },
+      el('path', { d: "m 22.5,0.7092993 c -0.56,-0.53 -1.27,-0.76 -2.13,-0.7 -0.56,0.06 -1.08,0.26 -1.58,0.6 -0.5,0.34 -1.05,0.7 -1.67,1.09 -0.62,0.39 -1.35,0.74 -2.2,1.07 -0.85,0.32 -1.91,0.49 -3.18,0.49 -1.3,0 -2.37,-0.15 -3.22,-0.46 -0.85,-0.31 -1.59,-0.66 -2.2,-1.04 -0.63,-0.4 -1.19,-0.77 -1.68,-1.12 -0.5,-0.36 -1.02,-0.56 -1.58,-0.63 -0.4,-0.03 -0.79,0.02 -1.16,0.14 -0.37,0.12 -0.7,0.32 -0.97,0.58 -0.28,0.26 -0.5,0.56 -0.67,0.9 -0.18,0.34 -0.26,0.71 -0.26,1.12 0,0.59 0.18,1.1 0.53,1.53 0.36,0.43 0.75,0.92 1.16,1.48 0.42,0.56 0.81,1.24 1.16,2.06 0.36,0.82 0.53,1.91 0.53,3.2699997 0,1.36 -0.18,2.46 -0.53,3.29 -0.36,0.83 -0.74,1.53 -1.16,2.09 -0.42,0.56 -0.8,1.04 -1.16,1.46 -0.35,0.42 -0.53,0.92 -0.53,1.51 0,0.8 0.29,1.48 0.88,2.02 0.59,0.54 1.32,0.78 2.18,0.72 0.56,-0.03 1.08,-0.22 1.58,-0.58 0.49,-0.36 1.05,-0.73 1.67,-1.11 0.62,-0.39 1.35,-0.74 2.2,-1.07 0.85,-0.32 1.93,-0.49 3.22,-0.49 1.27,0 2.33,0.16 3.18,0.46 0.85,0.31 1.58,0.67 2.2,1.07 0.62,0.4 1.18,0.77 1.67,1.11 0.49,0.34 1.02,0.54 1.58,0.6 0.9,0.09 1.63,-0.14 2.2,-0.7 0.57,-0.56 0.86,-1.24 0.86,-2.04 0,-0.59 -0.18,-1.09 -0.53,-1.51 -0.36,-0.42 -0.74,-0.9 -1.16,-1.46 -0.42,-0.56 -0.8,-1.25 -1.16,-2.09 -0.36,-0.83 -0.53,-1.93 -0.53,-3.29 0,-1.3599997 0.18,-2.4499997 0.53,-3.2699997 0.36,-0.82 0.74,-1.51 1.16,-2.06 0.42,-0.56 0.8,-1.05 1.16,-1.48 0.36,-0.43 0.53,-0.94 0.53,-1.53 -0.05,-0.83 -0.36,-1.51 -0.92,-2.03" } )
+    );
+
 	registerBlockType( 'xamoom/content', {
 		title: 'xamoom',
-		icon: 'shield-alt',
+		icon: iconEl,
 		category: 'embed',
 		supports: { multiple: false },
         attributes: {
@@ -197,7 +199,6 @@ function xamoomSelectContent(content_id){
         },
 
 
-		//
 		edit: function( props ) {
 		    var select = function(content_id, lang){
 		        var attributes = props.attributes;
@@ -296,14 +297,13 @@ function xamoomSelectContent(content_id){
                                         el(
                                             'span',
                                             { className: props.className, id: 'xamoom-selected-page-title' },
-                                            'hallo'
+                                            'loading'
                                         )
                                     ]
                                 )
                             )
                         ]
                   );
-
 		},
 
 		// The "save" property must be specified and must be a valid function.
